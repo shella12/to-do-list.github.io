e7829175f9d1471c5257c3bdbd05c43dc81c4d96
shella12 <ayeshaarshad4567@gmail.com>
2022-08-18 00:09:31 +0500

    Add to-do-list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import './style.css';
+import iconDots from './icon_dots.png';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = 'Hello webpack';
-    element.classList.add('hello');
-    return element;
+let todoArray=[
+  {
+    description:'Clean the car',
+    completed:false,
+    index:0
+  },
+  {
+    description:'Go for grocery shopping',
+    completed:false,
+    index:1
   }
+];
+
+const uList=document.body.querySelector('.ulist');
+
+function component(description,index) {
+    const list = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const checkboxImage = document.createElement('img');
+    list.className='chklist';
+    checkbox.name='todo';
+    checkbox.id=index;
+    checkbox.value=description;
+    checkbox.type='checkbox';
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.htmlFor=checkbox.id;
+    checkboxLabel.innerText=description;
+    checkboxImage.src=iconDots;
+    list.appendChild(checkbox);
+    list.appendChild(checkboxLabel);
+    list.appendChild(checkboxImage);
+    return list;
+  }
+
+todoArray.forEach(element => {
+const list=component(element.description,element.index);
+uList.appendChild(list);
+});
   
-  document.body.appendChild(component());
+  
