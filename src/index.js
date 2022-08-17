8f96b9ed8decc101932feafa411bc2a31fd9752a
shella12 <ayeshaarshad4567@gmail.com>
2022-08-17 19:36:53 +0500

    Add webpack configurations

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = 'Hello webpack';
+    element.classList.add('hello');
+    return element;
+  }
+  
+  document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode:'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'To-do-list',
+        template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
