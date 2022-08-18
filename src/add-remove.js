0b109690e3d4b40292a8a2846234acc835b7dd72
shella12 <ayeshaarshad4567@gmail.com>
2022-08-18 17:43:19 +0500

    Modify files

--- src/add-remove.js
+++ src/add-remove.js
@@ -2,20 +2,21 @@ import iconDots from './icon_dots.png';
 import iconDelete from './delete.png';
 import './style.css';
 
-const addList=document.body.querySelector('.add-list');
-const inputList=document.body.querySelector('.input-list');
+const addList = document.body.querySelector('.add-list');
+const inputList = document.body.querySelector('.input-list');
 const uList = document.body.querySelector('.ulist-child');
-export let todoArray = [];
+export const todoArray = [];
 
-addList.addEventListener('click',()=>{
-if(inputList.value !==''){
-const des=inputList.value;
-todoArray.push(
-  {description:des,completed:false}
-)
-}
-inputList.value='';
-createList();
+addList.addEventListener('click', () => {
+  if (inputList.value !== '') {
+    const des = inputList.value;
+    todoArray.push(
+      { description: des, completed: false },
+    );
+  }
+  inputList.value = '';
+  /* eslint-disable */
+  createList();
 });
 
 export function addTask(description, index) {
@@ -23,81 +24,76 @@ export function addTask(description, index) {
   const checkbox = document.createElement('input');
   const checkboxImage = document.createElement('img');
   list.className = 'chklist';
-  list.id=index;
+  list.id = index;
   checkbox.name = 'todo';
   checkbox.id = index;
   checkbox.value = description;
   checkbox.type = 'checkbox';
   const checkboxLabel = document.createElement('input');
-  checkboxLabel.type='text';
-  checkboxLabel.id='label'
+  checkboxLabel.type = 'text';
+  checkboxLabel.id = 'label';
   checkboxLabel.value = description;
-  checkboxLabel.readOnly=true;
+  checkboxLabel.readOnly = true;
   checkboxImage.src = iconDots;
-  checkboxImage.id='dots';
+  checkboxImage.id = 'dots';
   list.appendChild(checkbox);
   list.appendChild(checkboxLabel);
   list.appendChild(checkboxImage);
-  list.addEventListener('click',(event)=>{
-    editTask(event.target.id,list,checkboxImage,checkboxLabel);
+  list.addEventListener('click', (event) => {
+    /* eslint-disable */
+    editTask(event.target.id, list, checkboxImage, checkboxLabel);
   });
   return list;
 }
 
-export function removeTask(list){
-  
-const index=Number(list.id);
-list.remove();
-todoArray.splice(index,1);
-createList();
+export function removeTask(list) {
+  const index = Number(list.id);
+  list.remove();
+  todoArray.splice(index, 1);
+  /* eslint-disable */
+  createList();
 }
 
-export function editTask(event,list,checkboxImage,checkboxLabel){
-  
-    const index=Number(list.id);
+export function editTask(event, list, checkboxImage, checkboxLabel) {
+  const index = Number(list.id);
 
-    if(event==='label'){
-      
-      checkboxImage.src=iconDelete;
-      checkboxImage.id='delete';
-      checkboxLabel.readOnly=false;
-      checkboxLabel.id='labelClick';
-      list.style.backgroundColor='yellow';
-      checkboxLabel.style.backgroundColor='yellow';
-    }
-    else if(event==='delete'){
+  if (event === 'label') {
+    checkboxImage.src = iconDelete;
+    checkboxImage.id = 'delete';
+    checkboxLabel.readOnly = false;
+    checkboxLabel.id = 'labelClick';
+    list.style.backgroundColor = 'yellow';
+    checkboxLabel.style.backgroundColor = 'yellow';
+  } else if (event === 'delete') {
     removeTask(list);
-    }
-    else if(event==='labelClick'){
-      checkboxImage.src=iconDots;
-      checkboxImage.id='dots';
-      list.style.backgroundColor='white';
-      checkboxLabel.style.backgroundColor='white';
-      checkboxLabel.id='label';
-      checkboxLabel.readOnly=true;
-      todoArray[index].description=checkboxLabel.value;
-      localStorage.setItem('data',JSON.stringify(todoArray));
-    }
-    else{
-      checkboxImage.src=iconDots;
-      checkboxImage.id='dots';
-      list.style.backgroundColor='white';
-      checkboxLabel.style.backgroundColor='white';
-      checkboxLabel.id='label';
-      checkboxLabel.readOnly=true;
-      todoArray[index].description=checkboxLabel.value;
-      localStorage.setItem('data',JSON.stringify(todoArray));
-    }
-  
+  } else if (event === 'labelClick') {
+    checkboxImage.src = iconDots;
+    checkboxImage.id = 'dots';
+    list.style.backgroundColor = 'white';
+    checkboxLabel.style.backgroundColor = 'white';
+    checkboxLabel.id = 'label';
+    checkboxLabel.readOnly = true;
+    todoArray[index].description = checkboxLabel.value;
+    localStorage.setItem('data', JSON.stringify(todoArray));
+  } else {
+    checkboxImage.src = iconDots;
+    checkboxImage.id = 'dots';
+    list.style.backgroundColor = 'white';
+    checkboxLabel.style.backgroundColor = 'white';
+    checkboxLabel.id = 'label';
+    checkboxLabel.readOnly = true;
+    todoArray[index].description = checkboxLabel.value;
+    localStorage.setItem('data', JSON.stringify(todoArray));
+  }
 }
 
-export function createList(){
-localStorage.setItem('data',JSON.stringify(todoArray));
-uList.innerHTML='';
-  todoArray.forEach((element,index) => {
+function createList() {
+  localStorage.setItem('data', JSON.stringify(todoArray));
+  uList.innerHTML = '';
+  todoArray.forEach((element, index) => {
     const list = addTask(element.description, index);
     uList.appendChild(list);
-  });  
+  });
 }
 
 export default createList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 import createList, * as add from './add-remove.js';
 
-if(localStorage.getItem('data')!==null){
+if (localStorage.getItem('data') !== null) {
   const fetchDataList = JSON.parse(localStorage.getItem('data'));
- fetchDataList.forEach(element => {
-  add.todoArray.push(element);
- });
- 
-  add.createList();
+  fetchDataList.forEach((element) => {
+    add.todoArray.push(element);
+  });
+
+  createList();
 }
